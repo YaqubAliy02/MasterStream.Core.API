c60dfef953c064b5102dc47275220378f4ef585d
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-06-02 23:41:47 +0500

    DATA: Add VideoMetadata CRUD System

--- MasterStream.Core.API/Brokers/Storages/StorageBroker.VideoMetadatas.cs
+++ MasterStream.Core.API/Brokers/Storages/StorageBroker.VideoMetadatas.cs
@@ -10,5 +10,17 @@ namespace MasterStream.Core.API.Models.VideoMetadatas.Brokers.Storages
     internal partial class StorageBroker
     {
         private DbSet<VideoMetadata> VideoMetadatas {  get; set; }
+
+        public async ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata) =>
+            await InsertAsync(videoMetadata);
+
+        public IQueryable<VideoMetadata> SelectAllVideoMetadatas() =>
+            SelectAll<VideoMetadata>();
+
+        public async ValueTask<VideoMetadata> SelectVideoMetadataByIdAsync(Guid videometadataId) =>
+            await SelectAsync<VideoMetadata>(videometadataId);
+
+        public async ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata) =>
+            await DeleteAsync(videoMetadata);
     }
 }
--- MasterStream.Core.API/Brokers/Storages/StorageBroker.cs
+++ MasterStream.Core.API/Brokers/Storages/StorageBroker.cs
@@ -17,6 +17,36 @@ namespace MasterStream.Core.API.Models.VideoMetadatas.Brokers.Storages
             this.configuration = configuration;
             this.Database.Migrate();
         }
+
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private  IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
+
+        private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class =>
+            await this.FindAsync<T>(objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Modified;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+        
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Deleted;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = this.configuration.GetConnectionString("DefaultConnection");
