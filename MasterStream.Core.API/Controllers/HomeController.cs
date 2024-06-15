15b406d02c73af73748688fe26d716b551967956
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-06-15 11:53:29 +0500

    CONTROLLERS: Home Control

---
+++ MasterStream.Core.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//--------------------------
+// TARTEEB LLC               
+// ALL RIGHTS RESERVED      
+//--------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterStream.Core.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Thank you Mario! But the princess is in another castle!");
+
+    }
+}
--- MasterStream.Core.API/Program.cs
+++ MasterStream.Core.API/Program.cs
@@ -1,3 +1,5 @@
+using MasterStream.Core.API.Models.VideoMetadatas.Brokers.Storages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
