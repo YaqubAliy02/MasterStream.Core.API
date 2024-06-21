b1dd3c6e6359c28bed47921c04d22f4ee0810f29
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-21 17:47:16 +0500

    FOUNDATIONS: Retrieve All Videos

--- MasterStream.Core.API/Services/Videos/IVideoService.cs
+++ MasterStream.Core.API/Services/Videos/IVideoService.cs
@@ -1,7 +1,10 @@
-﻿namespace MasterStream.Core.API.Services.Videos
+﻿using MasterStream.Core.API.Models.Videos;
+
+namespace MasterStream.Core.API.Services.Videos
 {
     public interface IVideoService
     {
         Task<string> AddVideoAsync(Stream fileStream, string fileName, string contentType);
+        Task<List<Video>> RetrieveAllVideosAsync();
     }
 }
