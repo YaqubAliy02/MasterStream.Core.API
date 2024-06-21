834b8ce2ab46750ca596ff5d07dad5624460442d
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-21 21:57:57 +0500

    BROKERS: Insert Photo

---
+++ MasterStream.Core.API/Brokers/Blobs/IBlobBroker.Photo.cs
@@ -0,0 +1,12 @@
+﻿//--------------------------
+// TARTEEB LLC               
+// ALL RIGHTS RESERVED      
+//--------------------------
+
+namespace MasterStream.Core.API.Brokers.Blobs
+{
+    public partial interface IBlobBroker
+    {
+        Task<string> UploadPhotoAsync(Stream fileStream, string fileName, string contentType);
+    }
+}
