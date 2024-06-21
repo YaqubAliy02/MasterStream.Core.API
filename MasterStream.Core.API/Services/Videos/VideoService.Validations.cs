812d6a1026affe9b6773d8e53325aff723d31377
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-21 16:25:07 +0500

    FOUNDATIONS: Add Video

---
+++ MasterStream.Core.API/Services/Videos/VideoService.Validations.cs
@@ -0,0 +1,25 @@
+﻿using MasterStream.Core.API.Models.Exceptions;
+using MasterStream.Core.API.Models.VideoMetadatas;
+using MasterStream.Core.API.Models.Videos;
+using MasterStream.Core.API.Models.Videos.Exceptions;
+
+namespace MasterStream.Core.API.Services.Videos
+{
+    public partial class VideoService
+    {
+        public void ValidateVideo(IFormFile file)
+        {
+            if(file == null || !ValidateVideoExtensions(file))
+            {
+                throw new InvalidVideoExceptions("Video is not valid format.");
+            }
+        }
+        public bool ValidateVideoExtensions(IFormFile file)
+        {
+            var allowedExtensions = new[] { ".mp4", ".avi", ".mov" };
+            var extension = Path.GetExtension(file.FileName).ToLower();
+
+            return file.Length > 0 && file.Length <= 50 * 1024 * 1024 && allowedExtensions.Contains(extension);
+        }
+    }
+}
---
+++ MasterStream.Core.API/Services/Videos/VideoService.cs
@@ -0,0 +1,31 @@
+﻿//--------------------------
+// TARTEEB LLC               
+// ALL RIGHTS RESERVED      
+//--------------------------
+
+using MasterStream.Core.API.Brokers.Blobs;
+using MasterStream.Core.API.Brokers.DateTimes;
+using MasterStream.Core.API.Brokers.Loggings;
+
+namespace MasterStream.Core.API.Services.Videos
+{
+    public partial class VideoService : IVideoService
+    {
+        private readonly IBlobBroker blobBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public VideoService(
+            IBlobBroker blobBroker, 
+            ILoggingBroker loggingBroker, 
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.blobBroker = blobBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public async Task<string> AddVideoAsync(Stream fileStream, string fileName, string contentType) =>
+            await this.blobBroker.UploadVideoAsync(fileStream, fileName, contentType);
+    }
+}
