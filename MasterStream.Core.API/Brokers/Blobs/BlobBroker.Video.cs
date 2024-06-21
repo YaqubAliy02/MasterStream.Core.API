feef9865ff8fd372665cfcd27f7ce529f379aee8
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-21 14:03:25 +0500

    BROKERS: Select All Videos

--- MasterStream.Core.API/Brokers/Blobs/BlobBroker.Video.cs
+++ MasterStream.Core.API/Brokers/Blobs/BlobBroker.Video.cs
@@ -3,11 +3,47 @@
 // ALL RIGHTS RESERVED      
 //--------------------------
 
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using MasterStream.Core.API.Models.Videos;
+
 namespace MasterStream.Core.API.Brokers.Blobs
 {
     public partial class BlobBroker
     {
         public async Task<string> UploadVideoAsync(Stream fileStream, string fileName, string contentType) =>
             await UploadAsync(fileStream, fileName, contentType);
+
+        public async Task<List<Video>> GetAllVideosAsync()
+        {
+            var blobServiceClient = new BlobServiceClient(blobConnectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+            var blobItems = blobContainerClient.GetBlobsAsync();
+            var allowedExtensions = new[] { ".mp4", ".avi", ".mov" };
+
+            var videos = new List<Video>();
+
+            await foreach (BlobItem blobItem in blobItems)
+            {
+                var blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
+                var extension = Path.GetExtension(blobItem.Name);
+
+                if (allowedExtensions.Contains(extension))
+                {
+                    var properties = await blobClient.GetPropertiesAsync();
+
+                    videos.Add(new Video
+                    {
+                        Id = Guid.NewGuid(),
+                        FileName = blobItem.Name,
+                        ContentType = properties.Value.ContentType,
+                        Size = properties.Value.ContentLength,
+                        BlobUri = blobClient.Uri.ToString(),
+                        UploadedDate = properties.Value.CreatedOn.DateTime
+                    });
+                }
+            }
+            return videos;
+        }
     }
 }
--- MasterStream.Core.API/Brokers/Blobs/IBlobBroker.Video.cs
+++ MasterStream.Core.API/Brokers/Blobs/IBlobBroker.Video.cs
@@ -3,10 +3,13 @@
 // ALL RIGHTS RESERVED      
 //--------------------------
 
+using MasterStream.Core.API.Models.Videos;
+
 namespace MasterStream.Core.API.Brokers.Blobs
 {
     public partial interface IBlobBroker
     {
         Task<string> UploadVideoAsync(Stream fileStream, string fileName, string contentType);
+        Task<List<Video>> GetAllVideosAsync();
     }
 }
