7cbc9df3d5706ca06c96edc79a02d5588cadcc40
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-22 17:41:46 +0500

    BLAZOR: Initialize Blazor Project

---
+++ MasterStream.Web/Program.cs
@@ -0,0 +1,30 @@
+using MasterStream.Web.Components;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
