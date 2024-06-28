11344810383d8fa6af4e505f6644bd574055b05a
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-29 00:11:20 +0500

    BLAZOR: Get and Stream Videos

--- MasterStream.Core.API/Program.cs
+++ MasterStream.Core.API/Program.cs
@@ -39,7 +39,7 @@ internal class Program
                            .AllowAnyMethod();
                 });
         });
-
+        
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
             serverOptions.Limits.MaxRequestBodySize = 104857600;
